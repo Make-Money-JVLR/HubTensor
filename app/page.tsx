b697238e05f753cc0fc70078a28a1fbed9dc1da2
dawngn <haidangdavid123@gmail.com>
2024-03-03 03:24:25 +0700

    dev:fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Banner from "@/components/Banner.tsx";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-
+       <Banner />
     </main>
   );
 }
---
+++ components/Header/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Image from 'next/image'
+import logo from '@/images/logo.png'
+import Link from 'next/link'
+
+const Header = () => {
+
+  const menu = [
+    {
+      name: 'Home',
+      link: '/'
+    },
+    {
+      name: 'About',
+      link: '/about'
+    },
+    {
+      name: 'Services',
+      link: '/services'
+    },
+    {
+      name: 'Contact',
+      link: '/contact'
+    }
+  ]
+
+
+  return (
+    <header className='pt-[12px] pb-[16px] px-[60px] flex items-center justify-between  w-full mx-auto font-jetbrains h-[74px] border-b border-[#353539]'>
+      <Image src={logo} alt='logo' />
+      <ul className='flex items-center gap-[25px] h-full'>
+        {menu.map((item, index) => (
+          <li key={index} className='flex items-center gap-[25px] h-full px-[10px] py-[12px] text-[14px] leading-[22px] font-medium silver-text '>
+            <Link href={item.link} >
+              <span>{item.name}</span>
+            </Link>
+            <div className='h-full max-h-[20px] w-[1px] bg-[#353539]'></div>
+          </li>
+        ))}
+      </ul>
+      <div></div>
+    </header>
+  )
+}
+
+export default Header
