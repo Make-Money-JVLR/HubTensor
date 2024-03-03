893342cbccf9a89a4d7328177ec91af670ef0305
Chien Nguyen Quang <quangchieens1@gmail.com>
2024-03-03 13:45:12 +0700

    infra responsive

--- components/mission/index.tsx
+++ components/mission/index.tsx
@@ -35,23 +35,25 @@ function Mission() {
       backgroundSize: "cover",
     }} className="px-12 flex justify-center">
       <div className="max-w-[1440px]">
-        <div className="border-y-2 border-[#353539] flex justify-between py-8">
+        <div className="border-y-2 border-[#353539] md:flex md:justify-between py-8 space-y-4">
           <div className="space-y-4">
-            <Tag text={`HubTensor`} />
-            <div className="text-5xl font-segoe font-bold">
-              Our <span style={{
-                background: "var(--Gradient-Primary, linear-gradient(90deg,#C7B3F8 0%,#6C3BEF 50%,#4223A1 100%))",
-                backgroundClip: "text",
-                WebkitBackgroundClip: "text",
-                WebkitTextFillColor: "transparent"
-              }}>Infrastructure</span>
+            <div className="flex justify-center flex-col items-center gap-y-2 md:items-start">
+              <Tag text={`HubTensor`} />
+              <div className="text-3xl md:text-4xl lg:text-5xl font-segoe font-bold">
+                Our <span style={{
+                  background: "var(--Gradient-Primary, linear-gradient(90deg,#C7B3F8 0%,#6C3BEF 50%,#4223A1 100%))",
+                  backgroundClip: "text",
+                  WebkitBackgroundClip: "text",
+                  WebkitTextFillColor: "transparent"
+                }}>Infrastructure</span>
+              </div>
             </div>
           </div>
-          <div className="max-w-[429px]">
+          <div className="max-w-[429px] mx-auto md:mx-0">
             HubTensor <span className="text-[var(--Subtext,_#918DA4)] ">is an AI Infrastructure service provider that emphasizes ease of access and other additional features such as Privacy Service and App Chain which are our best products in blockchain and decentralized industry.</span>
           </div>
         </div>
-        <div className="flex justify-center gap-x-9 py-28" style={{
+        <div className="lg:flex lg:justify-center md:gap-x-9 md:grid md:grid-cols-2 lg:py-28 pb-8 space-y-8" style={{
         }}>
           {cards.map(({title, description, color, footer}, index) => (
             <Card title={title} key={index} description={description} footer={footer} color={color} />
