0addde7cd5a16710b0a3c78df8d9af0c8cf4d58e
dawngn <haidangdavid123@gmail.com>
2024-03-03 15:25:47 +0700

    dev:fix

--- app/template.tsx
+++ app/template.tsx
@@ -3,13 +3,12 @@ import { motion } from "framer-motion";
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
   <motion.div
-    initial={{ x: 300, opacity: 0 }}
-    animate={{ x: 0, opacity: 1 }}
-    exit={{ x: 300, opacity: 0 }}
-    transition={{
-      type: "spring",
-      stiffness: 260,
-      damping: 20,
+    initial="hidden"
+    whileInView="show"
+    viewport={{ once: true }}
+    variants={{
+      hidden: { opacity: 0 },
+      show: { opacity: 1, transition: { duration: 1 } },
     }}
   >
     {children}
