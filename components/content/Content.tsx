aecb93e3a606c4b57a05f2eca11887a459b01265
dawngn <haidangdavid123@gmail.com>
2024-03-03 13:26:59 +0700

    dev:fix

--- components/content/Content.tsx
+++ components/content/Content.tsx
@@ -9,7 +9,7 @@ function Content() {
     <div className="text-center space-y-8">
       <Tag text={`Our AI integration just landed`} icon center />
       <MainText />
-      <div className="text-[16px] text-[#FDFDFD] font-segoe leading-[22px]">Explore the Possibilities of AI Chatting with Omega</div>
+      <div className="md:text-[16px] text-[14px] text-[#FDFDFD] font-segoe md:leading-[22px] leading-[20px]">Explore the Possibilities of AI Chatting with Omega</div>
       <div className="flex justify-center gap-4">
         <Button content="Open Dapp" alt="Open Dapp" color="#000000" />
         <Button content="Read doc" alt="Read Doc" className="bg-black text-white" color="#ffffff" />
--- components/content/MainText.tsx
+++ components/content/MainText.tsx
@@ -1,9 +1,9 @@
 function MainText() {
   return (
-    <div className="font-segoe font-bold text-[48px] leading-[57.6px] main-text">
-      Unleash the power of AI within<br/>
-      <span className="primary-text">HubTensor.</span> Upgrade your<br/>
-      productivity with HubTensor, the<br/>
+    <div className="font-segoe font-bold md:text-[48px] text-[24px] md:leading-[57.6px] leading-[32px] main-text">
+      Unleash the power of AI within<br />
+      <span className="primary-text">HubTensor.</span> Upgrade your<br />
+      productivity with HubTensor, the<br />
       open AI chat App
     </div>
   )
